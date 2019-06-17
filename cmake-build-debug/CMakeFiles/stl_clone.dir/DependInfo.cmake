1bd76e37753c2985808d4a045dcf152e2225ff38
saberyjs <saberyjs@gmail.com>
2019-06-17 09:56:08 +0800

    cmake build

---
+++ cmake-build-debug/CMakeFiles/stl_clone.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/code/algorithm/list.c" "E:/code/algorithm/cmake-build-debug/CMakeFiles/stl_clone.dir/list.c.obj"
+  "E:/code/algorithm/main.c" "E:/code/algorithm/cmake-build-debug/CMakeFiles/stl_clone.dir/main.c.obj"
+  "E:/code/algorithm/tree.c" "E:/code/algorithm/cmake-build-debug/CMakeFiles/stl_clone.dir/tree.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/stl_clone.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stl_clone.dir/main.c.obj"
+  "CMakeFiles/stl_clone.dir/list.c.obj"
+  "CMakeFiles/stl_clone.dir/tree.c.obj"
+  "stl_clone.pdb"
+  "stl_clone.exe"
+  "stl_clone.exe.manifest"
+  "libstl_clone.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stl_clone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
