e29662a4dba66b3de5ba20817d606dae269c9f38
saberyjs <saberyjs@gmail.com>
2019-07-04 14:31:41 +0800

    cmake build

--- src/core/event.h
+++ src/core/event.h
@@ -51,7 +51,7 @@ static void EventHandleEvent(struct RbTreeNode *node);
 
 static void EventReInitLoop(struct EventDepositary *depositary);
 
-static void test(int n, struct fd_set *p) {
+static void test(int n, fd_set *p) {
     printf("fdbits:%lu\n", NFDBITS);
     //printf("test bit:%lu\n", (p)->fds_bits[(n) / NFDBITS]);
     printf("test bit:%lu\n", (p)->fds_bits[1]);
