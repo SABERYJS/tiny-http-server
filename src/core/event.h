ba688b9dd31234931a524e37870c4612c0cda70a
saberyjs <saberyjs@gmail.com>
2019-07-04 19:29:12 +0800

    cmake build

--- src/core/event.h
+++ src/core/event.h
@@ -7,9 +7,9 @@
 
 #include "global_header.h"
 
-#define EVENT_READABLE 1
-#define EVENT_WRITEABLE 2
-#define EVENT_ERROR 4
+#define EVENT_READABLE 1ul
+#define EVENT_WRITEABLE 2ul
+#define EVENT_ERROR 4ul
 
 typedef void (*eventHandleCallback)(int type, void *data);
 
