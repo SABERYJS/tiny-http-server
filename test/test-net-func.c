4574ff6bdc98e31e061fe8cd9d0fd33e1b2edeab
saberyjs <saberyjs@gmail.com>
2019-07-04 10:13:49 +0800

    cmake build

--- test/test-net-func.c
+++ test/test-net-func.c
@@ -3,8 +3,8 @@
 //
 
 
-#include <w32api/inaddr.h>
-#include <cygwin/in.h>
+/*#include <w32api/inaddr.h>
+#include <cygwin/in.h>*/
 #include "../src/core/net.h"
 #include <sys/select.h>
 #include "../src/algorithm/rb-tree.h"
