964a9c2139c65837df75f007ef669decec425712
saberyjs <saberyjs@gmail.com>
2019-07-13 19:41:10 +0800

    cmake build

--- src/core/cgi.h
+++ src/core/cgi.h
@@ -27,44 +27,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#ifndef STL_CLONE_CGI_H
+#define STL_CLONE_CGI_H
 
-#include "client.h"
-#include "buffer.h"
-
-struct Client *ClientCreate(int sock, struct sockaddr_in *clientAddr) {
-    struct Client *client = MemAlloc(sizeof(struct Client));
-    if (!client) {
-        return NULL;
-    } else {
-        client->sock = sock;
-        client->addr = clientAddr;
-        client->status = STATUS_RECEIVING_HEADER;
-        client->headers = HashTableCreate(100, NULL);
-        client->query = HashTableCreate(100, NULL);
-        client->request_line_parse_status = HTTP_REQUEST_LINE_METHOD;
-        if (!client->headers || !client->query) {
-            if (client->headers) {
-                MemFree(client->headers);
-            }
-            if (client->query) {
-                MemFree(client->query);
-            }
-            MemFree(client);
-            return NULL;
-        } else {
-            client->buffer = MemAlloc(sizeof(struct ClientBuffer));
-            if (!client->buffer) {
-                MemFree(client->headers);
-                MemFree(client);
-                return NULL;
-            } else {
-                client->buffer->write_pos = 0;
-                client->buffer->read_pos = 0;
-                memset(client->buffer->buf, 0, CLIENT_RECEIVE_BUFFER_SIZE);
-                client->buffer->size = 0;
-                client->buffer->left = 0;
-                return client;
-            }
-        }
-    }
-}
+#endif //STL_CLONE_CGI_H
--- test/test-cgi.c
+++ test/test-cgi.c
@@ -1,12 +1,46 @@
+/*Copyright (c) 2019 , saberyjs@gmail.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. All advertising materials mentioning features or use of this software
+   must display the following acknowledgement:
+   This product includes software developed by the <organization>.
+4. Neither the name of the <organization> nor the
+   names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <error.h>
 
-#define    CGI_NAME "E:\\PHPTutorial\\php\\php-7.2.1-nts\\php-cgi.exe"
-#define    REQUEST_METHOD "REQUEST_METHOD=POST"
-#define REQUEST_PARAMETER "myname=huangzhihui"
+#define    CGI_NAME "E:\\phpstudy\\PHPTutorial\\php\\php-7.2.1-nts\\php-cgi.exe"
+#define    REQUEST_METHOD "REQUEST_METHOD=GET"
+#define REQUEST_PARAMETER "mode=debug"
 
 int main(int argc, char *argv[]) {
     int fd[2];
@@ -21,9 +55,9 @@ int main(int argc, char *argv[]) {
         /* parent */
         //模拟向 CGI 传送数据
         ssize_t length = strlen(REQUEST_PARAMETER);
-        if (write(fd[1], REQUEST_PARAMETER, length) != length) {
+        /*if (write(fd[1], REQUEST_PARAMETER, length) != length) {
             printf("write error to pipe\n");
-        }
+        }*/
         close(fd[1]);
 
         //等待CGI子进程全然把数据读取后写入,
@@ -45,6 +79,9 @@ int main(int argc, char *argv[]) {
         }
         exit(0);
     } else {
+
+        chdir("E:\\phpstudy\\PHPTutorial\\WWW\\public\\");
+        int logf = open("./log.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
         /* child */
         //重定向管道的输入端到标准输入
         if (fd[0] != STDIN_FILENO) {
@@ -64,12 +101,13 @@ int main(int argc, char *argv[]) {
 
         //覆盖进程空间,设置CGI环境变量
         char content_length[128] = {0};
-        sprintf(content_length, "CONTENT_LENGTH=%u", strlen(REQUEST_PARAMETER));
-        char *exec_argv[3] = {REQUEST_METHOD, content_length};
-        printf("argc:%d\n", argc);
-        printf("argv[0] is %s\n", argv[0]);
-        if (execve(CGI_NAME, argv + 1, exec_argv) < 0) {
-            printf("execl error for %s", CGI_NAME);
+        char *exec_envp[7] = {"DOCUMENT_ROOT=E:\\phpstudy\\PHPTutorial\\WWW\\public\\", "SCRIPT_NAME=debug.php",
+                              "SCRIPT_FILENAME=E:\\phpstudy\\PHPTutorial\\WWW\\public\\debug.php"
+                              REQUEST_METHOD, "REDIRECT_STATUS=200", "SERVER_PROTOCOL=HTTP/1.1", NULL};
+        char *exec_argv[5] = {"php-cgi.exe", "-f", "debug.php", NULL};
+
+        if (execve(CGI_NAME, exec_argv, exec_envp) < 0) {
+            printf("execl error for %s", strerror(errno));
         }
         exit(0);
     }
